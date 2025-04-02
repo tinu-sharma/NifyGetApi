739ca85fa9312ea6e4fa14280c92f731b4323dc1
tinu-sharma <yudhishthirs738@gmail.com>
2025-04-02 13:33:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def download():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
   
