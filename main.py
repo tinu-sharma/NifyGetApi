a5037bafd403ff736c13f7cfaedc1e90982e2a82
tinu-sharma <yudhishthirs738@gmail.com>
2025-04-04 16:10:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,4 +69,5 @@ def download_video():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True
+    app.run(host="0.0.0.0", port=port, debug=True)
+    
